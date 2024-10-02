50d4c12008ab41a5a64ee6d32de0c5428abcc93a
Huub <50170696+huubl@users.noreply.github.com>
2024-10-02 16:34:19 +0200

    Revert "Navigation block: Use `apply_block_hooks_to_content()` (#65703)"
    This reverts commit 78dc0d4475ce22d9ad2b16d9756d445c9b895976.

--- packages/block-library/src/navigation/index.php
+++ packages/block-library/src/navigation/index.php
@@ -1499,15 +1499,9 @@ function block_core_navigation_mock_parsed_block( $inner_blocks, $post ) {
  */
 function block_core_navigation_insert_hooked_blocks( $inner_blocks, $post ) {
 	$mock_navigation_block = block_core_navigation_mock_parsed_block( $inner_blocks, $post );
-
-	if ( function_exists( 'apply_block_hooks_to_content' ) ) {
-		$mock_navigation_block_markup = serialize_block( $mock_navigation_block );
-		return apply_block_hooks_to_content( $mock_navigation_block_markup, $post, 'insert_hooked_blocks' );
-	}
-
-	$hooked_blocks        = get_hooked_blocks();
-	$before_block_visitor = null;
-	$after_block_visitor  = null;
+	$hooked_blocks         = get_hooked_blocks();
+	$before_block_visitor  = null;
+	$after_block_visitor   = null;
 
 	if ( ! empty( $hooked_blocks ) || has_filter( 'hooked_block_types' ) ) {
 		$before_block_visitor = make_before_block_visitor( $hooked_blocks, $post, 'insert_hooked_blocks' );
