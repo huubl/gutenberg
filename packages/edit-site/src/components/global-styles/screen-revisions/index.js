4889ff76edca9064bdfaa3d2d5485a09be6e9dce
Huub <50170696+huubl@users.noreply.github.com>
2024-10-02 16:34:19 +0200

    Revert "Global style revisions: remove unnecessary `goTo` navigation call (#65810)"
    This reverts commit 7b5f4b21d1d611b1c0dbd60cf04077ad0be29d65.

--- packages/edit-site/src/components/global-styles/screen-revisions/index.js
+++ packages/edit-site/src/components/global-styles/screen-revisions/index.js
@@ -72,6 +72,7 @@ function ScreenRevisions() {
 	);
 
 	const onCloseRevisions = () => {
+		goTo( '/' ); // Return to global styles main panel.
 		const canvasContainerView =
 			editorCanvasContainerView === 'global-styles-revisions:style-book'
 				? 'style-book'
